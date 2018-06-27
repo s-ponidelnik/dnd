572c425e9c34d0ffdf74420b0906f270e6ddcd32
sponidelnik <sponidelnik@aer.de>
2018-06-27 17:10:54 +0300

    c

--- src/Web/Controller/WebIndexController.php
+++ src/Web/Controller/WebIndexController.php
@@ -2,12 +2,27 @@
 
 namespace App\Web\Controller;
 
+use App\Core\Entity\Dice\DiceRoll;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class WebIndexController extends Controller
 {
+
+    /**
+     * @Route("/roll")
+     * @param Request $request
+     * @return JsonResponse
+     * @throws \Exception
+     */
+    public function rollAction(Request $request): JsonResponse
+    {
+        $roll = $request->get('roll');
+        return new JsonResponse(['result' => DiceRoll::rollDice($roll)]);
+    }
+
     /**
      * @Route("/")
      * @return JsonResponse
