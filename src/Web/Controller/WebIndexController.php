2ee181f9bccd8a47fda52babea579560478fd1c1
sponidelnik <sponidelnik@aer.de>
2018-07-19 11:08:06 +0300

    c

--- src/Web/Controller/WebIndexController.php
+++ src/Web/Controller/WebIndexController.php
@@ -12,6 +12,72 @@ class WebIndexController extends Controller
 {
 
     /**
+     * @Route("/races")
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function getRaces(Request $request): JsonResponse
+    {
+        $races = [];//TODO::get races info
+        return new JsonResponse(['result' => $races]);
+    }
+
+    /**
+     * @Route("/classes")
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function getClasses(Request $request): JsonResponse
+    {
+        $classes = [];//TODO::get classes info
+        return new JsonResponse(['result' => $classes]);
+    }
+
+    /**
+     * @Route("/feats")
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function getFeats(Request $request): JsonResponse
+    {
+        $feats = [];//TODO::get feats info
+        return new JsonResponse(['result' => $feats]);
+    }
+
+    /**
+     * @Route("/character")
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function getCharacter(Request $request): JsonResponse
+    {
+        $character = [];
+        return new JsonResponse(['result' => $character]);
+    }
+
+    /**
+     * @Route("/spells")
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function getSpells(Request $request): JsonResponse
+    {
+        $spells = [];
+        return new JsonResponse(['result' => $spells]);
+    }
+
+    /**
+     * @Route("/character-backgrounds")
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function getCharacterBackgrounds(Request $request): JsonResponse
+    {
+        $characterBackgrounds = [];
+        return new JsonResponse(['result' => $characterBackgrounds]);
+    }
+
+    /**
      * @Route("/roll")
      * @param Request $request
      * @return JsonResponse
